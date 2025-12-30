306d0a0d1cbed3202457b1c5f20bdd4f3514bdcd
lx090 <3357574363@qq.com>
2025-12-31 00:06:01 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,39 @@
 // 缓存版本
-const CACHE = 'my-app-v1';
+const CACHE = 'learning-assistant-v1';
 
-// 安装时缓存
+// 需要缓存的文件
+const CACHE_FILES = [
+  './',
+  './display-score.html',
+  './edit-score.html',
+  './style.css',
+  './service-worker.js'
+];
+
+// 安装时缓存所有必要文件
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(CACHE_FILES))
+  );
+});
+
+// 激活时清理旧缓存
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 // 优先使用缓存
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
   );
 });
